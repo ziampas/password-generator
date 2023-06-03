ae4ac4205ca3ce0217d8ce7004292f1bd3a8bdfa
Kristian Olausson <kristian.olausson@gmail.com>
2023-06-03 21:43:23 +0200

    UX changes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ import { nouns } from './nouns.js';
 function generatePassword() {
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  const specialChar = "!@#+";
-  const randomNumber = Math.floor(Math.random() * 90) + 10; // generates a random number between 10 and 99
-  const randomSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)];
+  const specialChars = "!@#+";
+  const randomNumber = Math.floor(Math.random() * 90) + 10; // Generates a random number between 10 and 99
+  const randomSpecialChar = specialChars[Math.floor(Math.random() * specialChars.length)];
 
   const password = adjective + noun + randomNumber + randomSpecialChar;
   const passwordLength = password.length;
@@ -18,6 +18,12 @@ const generateBtn = document.getElementById("generate-password");
 const generatedPasswordInput = document.getElementById("generated-password");
 const passwordLengthElement = document.getElementById("password-length");
 const copyBtn = document.getElementById("copy-to-clipboard");
+const alertDiv = document.getElementById("alertDiv");
+
+// Hide the alert div after 5 seconds
+setTimeout(() => {
+  alertDiv.style.display = "none";
+}, 5000); // 5 seconds in milliseconds
 
 generateBtn.addEventListener("click", () => {
   const { password, passwordLength } = generatePassword();
@@ -34,14 +40,14 @@ copyBtn.addEventListener("click", () => {
   generatedPasswordInput.select();
   document.execCommand("copy");
 
-  const alertDiv = document.createElement("div");
-  alertDiv.classList.add("alert", "alert-success", "my-3");
-  alertDiv.innerHTML = "Password has been copied!";
+  const successAlert = document.createElement("div");
+  successAlert.classList.add("alert", "alert-success", "my-3");
+  successAlert.textContent = "Password has been copied!";
 
   const parentElement = copyBtn.parentElement;
-  parentElement.insertBefore(alertDiv, copyBtn.nextSibling);
+  parentElement.insertBefore(successAlert, copyBtn.nextSibling);
 
   setTimeout(() => {
-    alertDiv.remove();
+    successAlert.remove();
   }, 3000);
 });
